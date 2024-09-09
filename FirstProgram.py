5e43063e2017bbf9162e260b3ade74e90501433b
Elicalpal <acalderonpalacios@unomaha.edu>
2024-09-09 14:03:42 -0500

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -12,11 +12,19 @@ def main():
   #Use the user's name in the program.
 
   #Ask the user for their age.
-
+  age=input ("How old are you you?" )
+  name=input("What is your name? ")
+  #Use the user's name in the program 
+  print ("Good to meet you")
+  age=int(age)
+  birthyear=2024-age
+  #Assume that they have not has their birthday yet this year
+  print(birthyear)
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
 
 
 #Call the main function if this is the file being run.
-if __name__ == '__main__':
-    main()
+#Call the main function if this the file being run.
+
+main()
